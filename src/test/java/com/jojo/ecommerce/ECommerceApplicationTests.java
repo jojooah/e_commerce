f40f25a65be6f7f66073aee529e8e55e0cb9aa0d
jojo <my_sg1@naver.com>
2025-07-24 01:31:28 +0900

    Initial commit

---
+++ src/test/java/com/jojo/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
