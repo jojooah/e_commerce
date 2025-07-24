d849bc87e46ccbb5dc5c92707e1a8f45b6ea2310
jojo <my_sg1@naver.com>
2025-07-24 22:30:43 +0900

    [port 구현 / 단위테스트코드 작성] 주문하기

---
+++ src/main/java/com/jojo/ecommerce/adapter/out/persistence/OrderRepositoryMap.java
@@ -0,0 +1,41 @@
+package com.jojo.ecommerce.adapter.out.persistence;
+
+import com.jojo.ecommerce.application.exception.OrderNotFoundException;
+import com.jojo.ecommerce.application.port.out.OrderRepositoryPort;
+import com.jojo.ecommerce.domain.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class OrderRepositoryMap implements OrderRepositoryPort {
+    private final Map<Long, Order> repository = new ConcurrentHashMap<>();
+    private Long sequence = 0L;
+
+    // 주문 저장
+    @Override
+    public Order saveOrder(Order order){
+        sequence++;
+        order.setOrderId(sequence);
+
+        repository.put(sequence, order);
+        return repository.get(sequence);
+    }
+
+    @Override
+    public Order updateOrder(Order order) {
+        if(repository.get(order.getOrderId()) == null){
+            throw new OrderNotFoundException(order.getOrderId());
+        }
+        repository.put(order.getOrderId(),order);
+
+        return order;
+    }
+
+    @Override
+    public Order findByOrderId(Long orderId){
+        return repository.get(orderId);
+    }
+
+}
---
+++ src/main/java/com/jojo/ecommerce/application/service/PlaceOrderService.java
@@ -0,0 +1,44 @@
+package com.jojo.ecommerce.application.service;
+
+import com.jojo.ecommerce.application.dto.CreateOrderRequest;
+import com.jojo.ecommerce.application.dto.ProductDto;
+import com.jojo.ecommerce.application.port.in.OrderUseCase;
+import com.jojo.ecommerce.application.port.out.OrderRepositoryPort;
+import com.jojo.ecommerce.domain.model.Order;
+import com.jojo.ecommerce.domain.model.OrderItem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceOrderService implements OrderUseCase {
+    private final OrderRepositoryPort orderRepositoryPort;
+
+    @Override
+    public Order placeOrder(CreateOrderRequest createOrderRequest){
+        // 주문 생성
+        Order order = new Order(createOrderRequest.getUserId());
+        Order saved = orderRepositoryPort.saveOrder(order);
+
+        // 상품 주문정보 돌면서 주문 아이템 생성
+        for(ProductDto productDto : createOrderRequest.getProductDtoList()){
+            order.addOrderItem(new OrderItem(order.getOrderId(),productDto.getProductId(), productDto.getQauntity()));
+        }
+
+        return orderRepositoryPort.saveOrder(order);
+    }
+
+    @Override
+    public Order cancelOrder(Long orderId){
+        Order order = orderRepositoryPort.findByOrderId(orderId);
+        order.paymentCanceled();
+
+        return orderRepositoryPort.updateOrder(order);
+    }
+
+    @Override
+    public Order selectOrder(Long orderId){
+        return orderRepositoryPort.findByOrderId(orderId);
+    }
+
+}
