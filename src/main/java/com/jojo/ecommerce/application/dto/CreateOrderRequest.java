6e1cafe636eafead458f30fab7bb8df0ca7b536b
jojo <my_sg1@naver.com>
2025-08-15 02:38:12 +0900

    [STEP10] 주문로직 동시성 고려하여 수정(레디스x), 요청토큰 생성

--- src/main/java/com/jojo/ecommerce/application/dto/CreateOrderRequest.java
+++ src/main/java/com/jojo/ecommerce/application/dto/CreateOrderRequest.java
@@ -9,6 +9,7 @@ import java.util.List;
 public record CreateOrderRequest (
      Long userId,
      Long couponId,
+     String requestId,
      int totalQuantity,
      int totalPrice,
      List<ProductDto> productDtoList
